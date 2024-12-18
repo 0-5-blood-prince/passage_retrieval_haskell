05bc0276df9d8fc5d881c232d76f4dcb9ccdb6d7
Samhit-Chowdary <samhit102@gmail.com>
2024-12-17 20:31:28 -0500

    update script

--- tf_idf/run_script.sh
+++ tf_idf/run_script.sh
@@ -4,12 +4,12 @@
 DIR_PATH="/Users/samhitbhogavalli/Documents/parallel_fuctional_programming/team_proj/20k/passages/"
 
 # Define the number of iterations
-ITERATIONS=1000
+CHUNK_SIZE=1000
 
 # Loop through CORES from 1 to 16
 for CORES in {1..16}
 do
     # Construct the command and run it with time
     echo "Running with $CORES cores..."
-    time ./tf_idf_v2_par "$DIR_PATH" "$ITERATIONS" +RTS -N$CORES -ls -s
+    time ./tf_idf_v2_par "$DIR_PATH" "$CHUNK_SIZE" +RTS -N$CORES -ls -s
 done
